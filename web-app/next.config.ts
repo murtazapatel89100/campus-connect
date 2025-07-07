d37349a955fa64b96b48410ef77c1e4a8342ee99
murtazapatel89100 <murtazapatel89100@gmail.com>
2025-07-07 16:50:08 +0530

    inital commit

---
+++ web-app/next.config.ts
@@ -0,0 +1,16 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
