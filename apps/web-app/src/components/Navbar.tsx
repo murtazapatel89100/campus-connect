e2f8db4cb668a509c8beea154011cae20ef606bc
murtazapatel89100 <murtazapatel89100@gmail.com>
2025-07-23 21:30:10 +0530

    add navbar

--- apps/web-app/src/components/Navbar.tsx
+++ apps/web-app/src/components/Navbar.tsx
@@ -1,12 +1,26 @@
 "use client";
-import {
-  VscAccount,
-  VscArchive,
-  VscHome,
-  VscSettingsGear,
-} from "react-icons/vsc";
-import Dock from "./ui/Dock";
 
-export default function Navbar() {
-  return <nav className="w-full bg-red-800">THIS IS NAVBAR</nav>;
+import { cn } from "../../lib/utils";
+
+type NavbarProps = {
+  className?: string;
+};
+
+export default function Navbar({ className }: NavbarProps) {
+  const navItemClass = cn(
+    "transition-transform hover:scale-125 hover:underline hover:text-teal-500 cursor-pointer"
+  );
+
+  return (
+    <nav
+      className={cn(
+        "fixed top-3 left-1/2 -translate-x-1/2 z-50 bg-gray-300 border border-black flex justify-center gap-10 p-4 w-[20vw] rounded-full font-semibold text-black",
+        className
+      )}
+    >
+      <p className={navItemClass}>HOME</p>
+      <p className={navItemClass}>ABOUT</p>
+      <p className={navItemClass}>CONTACT US</p>
+    </nav>
+  );
 }
