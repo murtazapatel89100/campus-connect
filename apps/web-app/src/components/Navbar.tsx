42e79b4d25c3d346b8cf3ac5a81cc5cacc9e5115
murtazapatel89100 <murtazapatel89100@gmail.com>
2025-07-23 22:01:15 +0530

    fix navbar

--- apps/web-app/src/components/Navbar.tsx
+++ apps/web-app/src/components/Navbar.tsx
@@ -8,19 +8,21 @@ type NavbarProps = {
 
 export default function Navbar({ className }: NavbarProps) {
   const navItemClass = cn(
-    "transition-transform hover:scale-125 hover:underline hover:text-teal-500 cursor-pointer"
+    "transition-transform hover:scale-125 hover:underline hover:text-teal-500 cursor-pointer whitespace-nowrap"
   );
 
   return (
-    <nav
-      className={cn(
-        "fixed top-3 left-1/2 -translate-x-1/2 z-50 bg-gray-300 border border-black flex justify-center gap-10 p-4 w-[20vw] rounded-full font-semibold text-black",
-        className
-      )}
-    >
-      <p className={navItemClass}>HOME</p>
-      <p className={navItemClass}>ABOUT</p>
-      <p className={navItemClass}>CONTACT US</p>
-    </nav>
+    <div className="fixed top-3 left-1/2 -translate-x-1/2 z-50">
+      <nav
+        className={cn(
+          "bg-gray-300 border border-black flex items-center justify-center gap-10 p-4 w-fit rounded-full font-semibold text-black",
+          className
+        )}
+      >
+        <p className={navItemClass}>HOME</p>
+        <p className={navItemClass}>ABOUT</p>
+        <p className={navItemClass}>CONTACT US</p>
+      </nav>
+    </div>
   );
 }
