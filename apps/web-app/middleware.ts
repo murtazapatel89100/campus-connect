9bf3bd0d7f223e34778bd2f6cdf2f822b6fa9015
murtazapatel89100 <murtazapatel89100@gmail.com>
2025-08-04 23:46:29 +0530

    updaet middle for nextjs

--- apps/web-app/middleware.ts
+++ apps/web-app/middleware.ts
@@ -1,12 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export default clerkMiddleware();
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/home", request.url));
+}
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: "/about/:path*",
 };
